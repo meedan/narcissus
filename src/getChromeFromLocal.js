16360c03e3033f1d044873d6b1434d259c6b21c0
Caio Almeida <caiosba@gmail.com>
2021-01-09 21:49:05 -0300

    Ticket CHECK-144: Fixing the error that reads: Could not find browser revision 800071. Run "PUPPETEER_PRODUCT=firefox npm install" or "PUPPETEER_PRODUCT=firefox yarn install" to download a supported Firefox browser binary.

--- src/getChromeFromLocal.js
+++ src/getChromeFromLocal.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 const getChromeFromLocal = async () => {
+  const browserFetcher = puppeteer.createBrowserFetcher();
+  const revisionInfo = await browserFetcher.download('809590.');
   const chrome = await puppeteer.launch({
+    executablePath: revisionInfo.executablePath,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     slowMo: process.env.SLOWMO_MS,
     dumpio: true,
